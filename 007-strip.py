10e27a76ed79c8526d5870631f52b148d1445b91
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-05 21:04:40 +0100

    Initial commit

---
+++ 007-strip.py
@@ -0,0 +1,10 @@
+# docs.python.org/3/library/stdtypes.html#string-methods
+
+# Ask user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from str
+name = name.strip()
+
+# Say hello to user
+print(f"hello, {name}!")
